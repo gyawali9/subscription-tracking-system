699ea664f55e347b86a16789d4e0274bebe123b4
gyawali9 <roshangyawali9@gmail.com>
2025-05-21 21:22:47 +0545

    feat: mongodb setup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -17,8 +18,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to subscriptio tracker");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`server running on port: ${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
