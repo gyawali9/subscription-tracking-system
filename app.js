24db6a877e536d0140f9da09fd6e16d611e4d493
gyawali9 <roshangyawali9@gmail.com>
2025-06-05 22:52:30 +0545

    feat: arcjet for rate limiter of api calls

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // Parses cookies from headers
 app.use(cookieParser());
+
+app.use(arcjetMiddleware);
 
 // like a middleware
 app.use("/api/v1/auth", authRouter);
