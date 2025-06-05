5908c39b442c9d9553fa78b3b668fe541b322a3c
gyawali9 <roshangyawali9@gmail.com>
2025-06-05 16:24:25 +0545

    feat: error handler middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 
@@ -6,13 +7,25 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+// This middleware parses incoming JSON payloads (e.g., Content-Type: application/json) and makes the data available on req.body.
+app.use(express.json());
+
+// Parses form URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+
+// Parses cookies from headers
+app.use(cookieParser());
 
 // like a middleware
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to subscriptio tracker");
